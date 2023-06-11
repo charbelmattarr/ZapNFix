d83abbd291786784ac6e3653622a0e902143d06a
charbelmattarr <74071943+charbelmattarr@users.noreply.github.com>
2023-06-11 11:09:04 +0300

    admin can create brand with foreign keyy

--- ZapNFix/ZapNFixApp/admin.py
+++ ZapNFix/ZapNFixApp/admin.py
@@ -1,17 +1,42 @@
 from django.contrib import admin
-
-from .models import Repair
-from .models import User
-from .models import Type
-from .models import Brand
-from .models import Component
-from .models import Problem
+from django.forms import inlineformset_factory
+from django import forms
+from django.forms.models import BaseInlineFormSet
+from .models import Repair, User, Type,Brand, Component, Problem
 
 
 # Register your models here.
 admin.site.register(Repair)
 admin.site.register(User)
 admin.site.register(Type)
-admin.site.register(Brand)
+#admin.site.register(Brand)
 admin.site.register(Component)
 admin.site.register(Problem)
+
+from django import forms
+from .models import Brand, Type
+
+from django import forms
+from .models import Brand
+
+class BrandForm(forms.ModelForm):
+    type_desc = forms.ModelChoiceField(queryset=Type.objects.all(), empty_label=None, to_field_name='desc')
+
+    class Meta:
+        model = Brand
+        fields = ['desc', 'type_desc']
+
+    def save(self, commit=True):
+        brand = super().save(commit=False)
+        type_desc = self.cleaned_data['type_desc']
+        # Create or get the Type instance based on the selected type_desc
+        type_instance, _ = Type.objects.get_or_create(desc=type_desc)
+        brand.type_id = type_instance
+        if commit:
+            brand.save()
+        return brand
+
+class BrandAdmin(admin.ModelAdmin):
+    form = BrandForm
+
+admin.site.register(Brand, BrandAdmin)
